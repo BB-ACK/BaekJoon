8a2fc75978aca9e5470e2714e9aca48bff5d3642
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-04-16 12:58:38 +0900

    merge 달팽이이

---
+++ Ch9_DivisorMultipleDecimal/Decimal_2581.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void decimal(int min, int max) {
+    
+}
+
+int main() {
+    int min; int max;
+    cin >> min >> max;
+
+    return 0;
+}

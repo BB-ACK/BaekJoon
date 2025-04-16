c3e4bb4ff1d0e3b2dc513c5f378199cbe6ff047d
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-04-16 17:36:19 +0900

    에디터 구현 중

---
+++ LinkedList/Editor_1406.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    char data; // 데이터 값
+    Node* Llink; // 앞쪽 노드 요소
+    Node* Rlink; // 뒤쪽 노드 요소
+};
+
+class DoubleLinkedList{
+public:
+    Node* cursor; // 커서의 역할
+    Node* top; // 제일 뒤 노드를 가리킴, 출력문을 위해
+    
+    // 생성자
+    DoubleLinkedList() {
+        this->cursor = NULL; 
+        this->top = NULL;
+    }
+
+    int isEmpty() {
+        return this->top == NULL;
+    }
+
+    // P에 대응 - 마지막에 요소를 넣어줌
+    void insert(char e) {
+        Node *node = new Node;
+        node->data = e;
+        
+        if(this->isEmpty()) {
+            node->Llink = NULL; // 맨 앞은 참조하는 왼쪽 노드가 없음
+            node->Rlink = NULL;
+            this->top = node;
+        }
+        
+        // 현재 커서가 제일 위에 있는 경우
+        else if(this->cursor->Rlink == NULL) {
+            // 새로운 노드 설정
+            node->Llink = this->cursor;
+            node->Rlink = NULL;
+            // 원래 노드 값들 변경
+            this->cursor->Rlink = node;
+            this->top = node;
+        }
+        else {
+            node->Llink = this->cursor;
+            node->Rlink = this->cursor->Rlink;
+            this->cursor->Rlink->Llink = node;
+            this->cursor->Rlink = node;
+        }
+
+        this->cursor = node; // 제일 위 노드를 새 노드로 교체
+    }
+
+    // L에 대응 - 커서를 왼쪽으로 옮김
+    void moveLeft() {
+        // 맨 앞이면 무시
+        if(this->cursor->Llink == NULL)
+            return;
+        this->cursor = this->cursor->Llink; // 왼쪽으로 옮김
+    }
+
+    // D에 대응 - 커서를 오른쪽으로 옮김
+    void moveRight() {
+        // 맨 끝이면 무시
+        if(this->cursor->Rlink == NULL) 
+            return;
+        this->cursor = this->cursor->Rlink; // 오른쪽으로 옮김
+    }
+
+    // B에 대응 - 현재 가리키는 커서 원소 삭제
+    void backSpace() {
+        // 맨 앞이면 무시
+        if(this->cursor->Llink == NULL)
+            return;
+        
+        Node prev = *this->cursor->Llink; // 커서 왼쪽 노드를 가리킴
+        if(this->cursor->Rlink == NULL) { // 커서가 맨위에 있는 경우
+            prev.Rlink = NULL;
+        }
+        else {
+            prev.Rlink = this->cursor->Rlink;
+        }
+        
+        this->cursor = &prev;
+    }
+
+};
+
+int main() {
+    DoubleLinkedList DLL;
+    string element; cin >> element;
+
+    for(char idx : element) {
+        DLL.insert(idx);
+    }
+
+    int num; cin >> num;
+    while(num--) {
+        char instructions; cin >> instructions;
+
+        switch (instructions) {
+        case 'L':
+            DLL.moveLeft();
+            break;
+        case 'D':
+            DLL.moveRight();
+            break;
+        case 'B':
+            DLL.backSpace();
+            break;
+        case 'P':
+            char e; cin >> e;
+            DLL.insert(e);
+            break;
+        }
+    }
+
+    Node *tmp = DLL.top;
+    while(tmp->Llink != NULL) {
+        cout << tmp->data;
+        tmp = tmp->Rlink; // 다음 노드로 변경
+    }
+
+    cout << endl;
+
+    return 0;
+}

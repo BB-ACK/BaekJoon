6411f2d1c57b08fc98497b9427afab9eca0cb335
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-04-18 23:27:32 +0900

    소수 solved

--- Ch9_DivisorMultipleDecimal/Decimal_2581.cpp
+++ Ch9_DivisorMultipleDecimal/Decimal_2581.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
-void decimal(int min, int max) {
+void findDecimal(int start, int end) {
+    int first = -1;
+    int sum = 0;
     
+    for(int i = start; i <= end; i++) {
+        if(i == 2) {
+            first = i;
+            sum += i;
+            continue;
+        }
+        for (int j = 2; j < i; j++) {
+            
+            if(i % j == 0)
+                break;
+
+            if(j == i - 1) {
+                sum += i;
+                if(first == -1)
+                    first = i;
+            }
+        }
+
+    }
+
+    if(sum != 0)
+        cout << sum << endl;
+    cout << first << endl;
 }
 
-int main() {
-    int min; int max;
-    cin >> min >> max;
+int main()
+{
+    int start, end;
+    cin >> start >> end;
+
+    findDecimal(start, end);
 
     return 0;
 }

3b51d7a3ec62ad64682378061dfb6d22f0a1628d
bsh1706 <84366169+bsh1706@users.noreply.github.com>
2025-05-21 15:36:39 +0900

    최소힙 sovled - by 완전이진트리

--- PriorityQueue/MinHeap_1927.cpp
+++ PriorityQueue/MinHeap_1927.cpp
@@ -1,69 +1,98 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 using namespace std;
 
-class PriorityQueue {
+class MinHeap {
 private:
-    vector<int> arr;
+    deque<int> tree;
+    int size;
 
 public:
     // 생성자
-
-    int isEmpty() {
-        return this->arr.size() == 0;
+    MinHeap() {
+        this->tree.push_back(NULL); // 0번 인덱스를 사용하지 않기 위함함
+        this->size = 0;
     }
 
-    // 우선순위를 정하기 위해
-    int findMinIdx() {
-        long long min = INT32_MAX;
-        int idx = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] < min) {
-                min = arr[i];
-                idx = i;
-            }
+    int isEmtpy() {
+        return this->size == 0;
+    }
+
+    // 원소 삽입 함수
+    void insert(int e) {
+        this->size++;
+        this->tree.push_back(e);
+        this->upHeap();
+    }
+
+    // 삽입된 원소에 위치를 찾는 함수
+    void upHeap() {
+        int index = this->size;
+        int key = this->tree[index];
+
+        // 루트 노드가 아니고 부모 노드보다 작다면면
+        while(this->size != 1 && key < this->tree[index / 2]) {
+            this->tree[index] = this->tree[index / 2]; // 부모노드와 교체
+            index /= 2;
         }
 
-        return idx;
+        this->tree[index] = key;
     }
 
-    // 정수가 입력됐을 때
-    void push(int element) {
-        arr.push_back(element);
-    }
-
-    // 0이 입력됐을 때
-    void pop() {
-        if(this->isEmpty()) {
-            cout << 0 << '\n';
-            return;
-        }
+    // 원소 삭제 함수
+    int remove() {
+        if(this->isEmtpy())
+            return 0;
         
-        int idx = this->findMinIdx();
-        cout << arr[idx] << "\n";
-        arr.erase(arr.begin() + idx);
+        int key = this->tree[1];
+        this->tree[1] = this->tree.back();
+        this->tree.pop_back(); 
+        // 여기까지 맨 앞요소와 바꾸고 버림
+        
+        this->size--;
+        this->downHeap();
+
+        return key;
+    }
+
+    void downHeap() {
+        int key = this->tree[1];
+        int pi = 1; int ci = 2; // 기본적으로 자식 노드는 왼쪽 - 2i
+
+        while(ci <= this->size) { // 자식노드가 있어야함
+            if(ci < this->size && this->tree[ci+1] < this->tree[ci]) { // 형제 노드가 존재하고 오른쪽 노드가 큰경우
+                ci++; // 오른쪽 노드로 변경
+            }
+
+            // 자신이 더 작으면 내려갈 필요가 없음
+            if(key < this->tree[ci])
+                break;
+
+            this->tree[pi] = this->tree[ci];
+            pi = ci; // 부모노드가 자식 노드로 내려감
+            ci *= 2; // 바뀐 부모 노드의 값에 맞춰 자식 노드 인덱스 값 변경
+        }
+
+        this->tree[pi] = key;
     }
 };
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     
     int test; cin >> test;
-    PriorityQueue pq;
+    MinHeap H;
 
     while(test--) {
         int cmd; cin >> cmd;
 
-        switch (cmd){
-        case 0:
-            pq.pop();
-            break;
-        default:
-            pq.push(cmd);
-            break;
+        if(cmd == 0) 
+            cout << H.remove() << '\n';
+        else {
+            H.insert(cmd);
         }
     }
+
     return 0;
 }
